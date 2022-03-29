4118f850d66c9a05623ed4ffcc5b685bee5ae7e1
Sajid576 <sajidahmed696@gmail.com>
2022-03-29 16:50:31 +0600

    Refactor

--- run.sh
+++ run.sh
@@ -6,19 +6,19 @@ RUNPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 python3 $PWD/scripts/resize.py $RUNPATH dim=5000,3000 dataset_folder=sample2
 
-# echo $RUNPATH
-# sudo docker run -ti --rm -v $RUNPATH/datasets:/datasets opendronemap/odm --project-path /datasets project --dsm --orthophoto-resolution 4 --auto-boundary
+echo $RUNPATH
+sudo docker run -ti --rm -v $RUNPATH/datasets:/datasets opendronemap/odm --project-path /datasets project --dsm --orthophoto-resolution 4 --auto-boundary
 
-# # unlock all result folders
-# sudo chmod 777 -R $RUNPATH/datasets/project
-# # cd $RUNPATH/datasets/project
+# unlock all result folders
+sudo chmod 777 -R $RUNPATH/datasets/project
+# cd $RUNPATH/datasets/project
 
-# # #clear previous results
-# rm -r $RUNPATH/results/*
+# #clear previous results
+rm -r $RUNPATH/results/*
 
-# # move results
-# shopt -s extglob
-# mkdir $RUNPATH/datasets/project/results/
-# mv -vt $RUNPATH/datasets/project/results/ $RUNPATH/datasets/project/!(images)
-# # move results to the root directory
-# mv -vt $RUNPATH $RUNPATH/datasets/project/results/
+# move results
+shopt -s extglob
+mkdir $RUNPATH/datasets/project/results/
+mv -vt $RUNPATH/datasets/project/results/ $RUNPATH/datasets/project/!(images)
+# move results to the root directory
+mv -vt $RUNPATH $RUNPATH/datasets/project/results/
